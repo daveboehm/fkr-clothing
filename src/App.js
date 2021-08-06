dfee363b20d603a20bf09654042476ee9670dc6d
dave boehm <davidnboehm@gmail.com>
2021-08-06 16:45:15 -0500

    Adds firebase authentication

--- src/App.js
+++ src/App.js
@@ -4,17 +4,42 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import { Switch, Route } from 'react-router-dom'
 import { Header } from 'components/header/header.component'
-   
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
-    </div>
-  );
+import { AuthPage } from 'components/auth/auth.component'  
+import { auth } from 'firebase/firebase.utils'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    const setUser = (user) => this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(setUser)
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/sign-in' component={AuthPage} />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,9 @@ import './header.styles.scss'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo} from 'assets/svg/clothes-hanger.svg'
 import { ReactComponent as Cart} from 'assets/svg/shopping-cart.svg'
+import { auth } from 'firebase/firebase.utils'
 
-export const Header = () => (
+export const Header = ({ currentUser }) => (
     <header className="header">
         <Link to="/">
             <Logo className="logo" />
@@ -12,6 +13,10 @@ export const Header = () => (
         <div className="options">
             <Link className="option" to="/shop">Shop</Link>
             <Link className="option" to="/contact">Contact</Link>
+            {currentUser
+                ? <div className="option" onClick={() => auth.signOut()}>Sign Out</div>
+                : <Link className="option" to="/sign-in">Sign In</Link> 
+            }
             <Link className="option" to="/cart">
                 <Cart className="cart" />
             </Link>
